2d83eabc827b00fbc9ccc66754d97b1d70dca8e2
unknown <vasilisium@gmail.com>
2018-11-14 16:47:39 +0200

    created frontend app

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,16 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+
+import {MaterialModule} from './material.components'
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavComponent
   ],
   imports: [
+    
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
