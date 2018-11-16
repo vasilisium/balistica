7100c87237f0b6e0b1bcd4b11077cd5953e5688e
unknown <vasilisium@gmail.com>
2018-11-16 15:35:02 +0200

    zxc

--- frontend/src/app/material.components.ts
+++ frontend/src/app/material.components.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar'
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-import {MatTableModule} from '@angular/material/table';
+
 // import {MatPaginator, MatTableDataSource, MatSort} from '@angular/material';
 
 
@@ -11,10 +11,7 @@ var modules = [
     MatToolbarModule, 
     MatIconModule, 
     MatButtonModule, 
-    MatTableModule,
-    // MatPaginator,
-    // MatTableDataSource,
-    // MatSort,
+    ,
 ]
 
 @NgModule ({
